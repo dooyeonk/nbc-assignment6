03e6ec4b9882a5202e34b5229c0084fea7efc5cc
강두연 <dyhouse0517@gmail.com>
2026-01-12 19:53:03 +0900

    chore: project init

---
+++ Source/NbcAssignment6/NbcAssignment6.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NbcAssignment6.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NbcAssignment6, "NbcAssignment6" );
