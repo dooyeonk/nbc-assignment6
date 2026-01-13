e601cdd5ee38f6ad2fc98d00e7d2654797cb59bb
강두연 <dyhouse0517@gmail.com>
2026-01-13 19:34:47 +0900

    feat: 셔틀 무빙 컴포넌트 추가

---
+++ Source/NbcAssignment6/Private/ShuttleMoveComponent.cpp
@@ -0,0 +1,64 @@
+#include "ShuttleMoveComponent.h"
+
+UShuttleMoveComponent::UShuttleMoveComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UShuttleMoveComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AActor* Owner = GetOwner())
+	{
+		StartLocation = Owner -> GetActorLocation();
+	}
+	
+	UpdatesPerSecond = UpdatesPerSecond > 0.0f ? UpdatesPerSecond : 60.0f;
+	
+	ActualInterval = 1.0f / UpdatesPerSecond;
+	
+	GetWorld()->GetTimerManager().SetTimer(
+		MoveTimerHandle,
+		this,
+		&UShuttleMoveComponent::UpdateMovement,
+		ActualInterval,
+		true
+	);
+}
+
+bool UShuttleMoveComponent::IsMovingReverse() const
+{
+	return Direction <= 0.0f;
+}
+
+void UShuttleMoveComponent::UpdateMovement()
+{
+	AActor* Owner = GetOwner();
+	if (!Owner) return;
+	
+	FVector CurrentLocation = Owner->GetActorLocation();
+	FVector TargetDestination = IsMovingReverse() ? StartLocation : StartLocation + MaxRange;
+	
+	FVector MoveNormal = (TargetDestination - CurrentLocation).GetSafeNormal();
+	CurrentLocation += MoveNormal * Velocity * ActualInterval;
+	Owner->SetActorLocation(CurrentLocation);
+
+	if (FVector::Dist(CurrentLocation, TargetDestination) <= (Velocity * ActualInterval))
+	{
+		Owner->SetActorLocation(TargetDestination);
+		Direction *= -1.0f;
+	}
+}
+
+void UShuttleMoveComponent::SetMovementActive(bool bActive) const
+{
+	if (bActive)
+	{
+		GetWorld()->GetTimerManager().UnPauseTimer(MoveTimerHandle);
+	}
+	else
+	{
+		GetWorld()->GetTimerManager().PauseTimer(MoveTimerHandle);
+	}
+}
---
+++ Source/NbcAssignment6/Public/ShuttleMoveComponent.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ShuttleMoveComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NBCASSIGNMENT6_API UShuttleMoveComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UShuttleMoveComponent();
+
+	void SetMovementActive(bool bActive) const;
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category = "Movement Settings")
+	float Velocity = 500.0f;
+	
+	UPROPERTY(EditAnywhere, Category = "Movement Settings")
+	FVector MaxRange = FVector(500.0f, 0.0f, 0.0f);
+	
+	UPROPERTY(EditAnywhere, Category = "Movement Settings", meta = (ClampMin = "1.0"))
+	float UpdatesPerSecond = 60.0f;
+	
+private:	
+	void UpdateMovement();
+	
+	FTimerHandle MoveTimerHandle;
+	FVector StartLocation;
+	float Direction = 1.0f;
+	float ActualInterval;
+	
+	bool IsMovingReverse() const;
+};
